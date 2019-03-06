b417a5f56a2dd61db284f549bef976dd1da12cd1
Amit Gupta <amitgupta@Amits-iMac.local>
2019-03-06 16:01:32 +0530

    Added Modal component

--- FE/src/index.js
+++ FE/src/index.js
@@ -4,12 +4,30 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import FaIcon from "./components/FaIcon";
 import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
 
 import "./index.scss";
 
 ReactDOM.render(
   <div>
-    <Navbar></Navbar>
+    
+    <Modal className="active">
+    <a href="#close" className="modal-overlay" aria-label="Close"></a>
+  <div className="modal-container">
+    <div className="modal-header">
+      <a href="#close" className="btn btn-clear float-right" aria-label="Close"></a>
+      <div className="modal-title h5">Modal title</div>
+    </div>
+    <div className="modal-body">
+      <div className="content">
+       Hello
+      </div>
+    </div>
+    <div className="modal-footer">
+      
+    </div>
+  </div>
+    </Modal>
   </div>,
   document.querySelector("#root")
 );
