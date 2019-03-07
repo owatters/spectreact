81fe1dcf872282f6da0cf5108d80d05729c2fcaa
Prashant <prashant@owatters.com>
2019-03-07 13:52:38 +0530

    Modal component

---
+++ components/modalOverlay/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./style.scss";
+
+export default class ModalOverlay extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={(this.props.className != null ? this.props.className : "") + " modal-overlay"}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Select from "./components/Select";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Modal from "./components/Modal";
+import ModalOverlay from "./components/modalOverlay";
+import ModalContainer from "./components/modalContainer";
+import ModalHeader from "./components/modalHeader";
+import ModalBody from "./components/modalBody";
+import ModalFooter from "./components/modalFooter";
 
-//ReactDOM.render(<Input className="primary"> Test </Input> , document.getElementById("root"))
-
-ReactDOM.render(<Select className="hello" options={['Hello','Hi'] }/> , document.getElementById("root"))
+ReactDOM.render(
+    <div>hello</div>
+    // <Modal className="primary">
+    //     <ModalOverlay />
+    //     <ModalContainer>
+    //         <ModalHeader>Demo title</ModalHeader>
+    //         <ModalBody> content</ModalBody>
+    //         <ModalFooter>Footer</ModalFooter>
+    //     </ModalContainer>
+    // </Modal>
+    , document.getElementById("root"))
