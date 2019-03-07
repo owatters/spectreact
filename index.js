40b2096de8db91164a49933d4155ba34467d3c6e
Waseem <waseem@owatters.com>
2019-03-07 14:11:47 +0530

    .gitignore shifted to dist

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import Select from "./components/Select";
 
 //ReactDOM.render(<Input className="primary"> Test </Input> , document.getElementById("root"))
 
-ReactDOM.render(<Select className="hello" options={['Hello','Hi'] }/> , document.getElementById("root"))
+ReactDOM.render(
+    <div>hello</div>, document.getElementById("root"))
