2304f844a883ad24dc32324e0065227ccc517ae2
Waseem <waseem@owatters.com>
2019-03-07 13:54:05 +0530

    button onclick added

--- components/Button/index.js
+++ components/Button/index.js
@@ -4,18 +4,17 @@ import ReactDOM from "react-dom";
 import "./style.scss";
 
 export default class Button extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        className: this.props.className ? this.props.className : "",
-      };
-    }
-    render() {
-      return (
-        <button className={this.state.className + " button"}>
-          {this.props.children}
-        </button>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      className: this.props.className ? this.props.className : "",
+    };
   }
-  
+  render() {
+    return (
+      <button className={this.state.className + " button"} onClick={this.props.onclick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
