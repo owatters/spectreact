ed576e2fbff9cf352e114bfb4b9c82cceccb249f
rajeev <rajeev@owatters.com>
2019-03-07 17:44:15 +0530

    Loader component

---
+++ components/Loader/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import "./style.scss";
+
+export default class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      className: this.props.className ? this.props.className : "",
+    };
+  }
+  render() {
+    return (
+      <button className={this.state.className + " loading"} onClick={this.props.onclick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
